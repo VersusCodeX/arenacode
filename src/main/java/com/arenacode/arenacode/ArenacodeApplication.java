81a17d8b7841996cfea27d8a792db2487e8371ac
Andre_Manjaro <luizzz.z127@gmail.com>
2026-09-08 17:02:05 -0300

    feat: initialize Spring Boot application with Gradle setup

---
+++ src/main/java/com/arenacode/arenacode/ArenacodeApplication.java
@@ -0,0 +1,13 @@
+package com.arenacode.arenacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenacodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenacodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arenacode/arenacode/ArenacodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arenacode.arenacode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenacodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
