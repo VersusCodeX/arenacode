63c3ba4aadd9bd3c50c6d9f86d357354e333ba0e
André Luiz <75501398+AnDrELuIzzz@users.noreply.github.com>
2026-09-17 00:09:50 -0300

    fix(identity): remove @Configuration from JwtConfig

--- src/main/java/com/arenacode/arenacode/identity/config/JwtConfig.java
+++ src/main/java/com/arenacode/arenacode/identity/config/JwtConfig.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
@@ -18,7 +17,6 @@ import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import com.nimbusds.jose.proc.SecurityContext;
 
-@Configuration
 @EnableConfigurationProperties(JwtProperties.class)
 public class JwtConfig {
 
